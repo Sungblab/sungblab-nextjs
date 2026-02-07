94dedfdcdc76d5ba6751e5f9cd94ac084e2c0f02
Sungblab <98207899+Sungblab@users.noreply.github.com>
2026-02-07 18:52:21 +0900

    디플로이 오류 수정

--- tina/config.ts
+++ tina/config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "tinacms";
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '.env.local' });
+
 
 // Your hosting provider likely exposes this as an environment variable
 const branch =
